d317c1b571714ad1e1ddeb44e6066e1c58a5bb91
Omran Alhaddad <44875477+Omrna@users.noreply.github.com>
2019-11-29 21:29:36 +0000

    Add files via upload

--- convolution.cpp
+++ convolution.cpp
@@ -5,123 +5,192 @@
 #include <opencv/cxcore.h>    //depending on your machine setup
 #include <cmath>
 #include<bits/stdc++.h>
-
+#include <vector>
 using namespace cv;
 using namespace std;
 
 #define pi 3.14159265
 
-double find_min(Mat x);
-double find_max(Mat x);
-void normolization(Mat x,Mat y,double min_x,double max_x);
+//void normolization(Mat x,Mat y,double min_x,double max_x);
+//void getMag(Mat &Gx,Mat &Gy,Mat &dst,Mat &img);
+//void getDirection(Mat &Gx,Mat &Gy,Mat &dst);
+void getThresholdedMag(Mat &input, Mat &output);
+
 int main( int argc, char** argv )
 {
  // LOADING THE IMAGE
  Mat image;
  image = imread( "detected.jpg", 1 );
-
+ image.convertTo(image, CV_32F);
+ Mat image2 = image.clone();
  if( argc != 1 || !image.data)
  {
    printf( " No image data \n " );
    return -1;
  }
-
+ GaussianBlur( image, image, Size(3,3), 0, 0, BORDER_DEFAULT );
  // CONVERT COLOUR, BLUR AND SAVE
  Mat gray_image;
+ gray_image.create(image.size(), CV_32F);
  cvtColor( image, gray_image, CV_BGR2GRAY );
- Mat gray_image2;
- cvtColor( image, gray_image2, CV_BGR2GRAY );
 
  // instilize two filtires dx and dy
- int d1[9] = {-1, 0, 1, -2, 0, 2, -1, 0, 1};
- int d2[9] = {1, 2, 1, 0, 0, 0, -1, -2, -1};
- cv::Mat dx= cv::Mat(3, 3, CV_32S, d1);
- cv::Mat dy= cv::Mat(3, 3, CV_32S, d2);
+ Mat dx,dy,dxKernel,dyKernel;
+ dx.create(image.size(), CV_32F);
+ dy.create(image.size(), CV_32F);
 
- // Apply the dx and dy filtiers on the gray image
- //Gx= dx * gray_image
- //Gy= dy * gray_image
+ dxKernel = (Mat_<float>(3,3) << -1, 0, 1,
+                                     -2, 0, 2,
+                                     -1, 0, 1);
+
+ dyKernel = (Mat_<float>(3,3) << -1,-2,-1,
+                                      0, 0, 0,
+                                      1, 2, 1);
+
  Mat Gx,Gy;
+ Gx.create(image.size(), CV_32F);
+ Gy.create(image.size(), CV_32F);
  Point anchor=Point( -1, -1 );
  double delta=0;
- filter2D(gray_image, Gx, 5 , dx, anchor, delta, BORDER_DEFAULT );
- filter2D(gray_image, Gy, 5 , dy, anchor, delta, BORDER_DEFAULT );
+ filter2D(gray_image, Gx, 5 , dxKernel, anchor, delta, BORDER_DEFAULT );
+ filter2D(gray_image, Gy, 5 , dyKernel, anchor, delta, BORDER_DEFAULT );
 
+ //outMag.create(gray_image.size(),CV_64F);
+ //Mat outDir,outMag,magthresh;
+ //outDir.create(image.size(), CV_32F);
+ //outDir.create(gray_image.size(),CV_32F);
+
+ //Mat outDir.create(gray_image.size(),CV_64F);
+ //outMag.create(gray_image.size(),CV_32F);
+ //magthresh.create(gray_image.size(),CV_32F);
+ //getMag(Gx,Gy,outMag,gray_image);
+ //getDirection(Gx,Gy,outDir);
+ //imwrite( "outMag.jpg", outMag);
  //instilize  Matrices
- cv::Mat mag(Gx.size(), Gx.type());
- cv::Mat theta(mag.size(), mag.type());
-
- cv::Mat rho(Gx.size(), Gx.type());
- cv::Mat houghspaces;
-
- //int rho_length = 2 * (rho_width+rho_height);
- //int theta_length = 360;
- //calculating the Magnitude which is: mag=sqrt(Gx^2 + Gy^2)
- cv::magnitude(Gx, Gy, mag);
-
-
- int rho_width = rho.rows;
- int rho_height = rho.cols;
- houghspaces.create(2*(rho_width+rho_height),360,CV_64F);
+ cv::Mat mag,theta,houghspaces;
+ //mag.create(gray_image.size(),CV_64F);
+ //theta.create(gray_image.size(),CV_64F);
+ //rho.create(gray_image.size(),CV_32F);
+ //MagThresh.create(gray_image.size(),CV_64F);
+ int rho_width = gray_image.rows;
+ int rho_height = gray_image.cols;
+ houghspaces.create(2*(rho_width+rho_height),360,CV_32F);
  houghspaces = Scalar(0,0,0);
 
- Mat mag3, angle3;
- cartToPolar(Gx, Gy, mag3, angle3, 1);
+ //cv::Mat rho(Gx.size(), Gx.type());
+ //calculating the Magnitude which is: mag=sqrt(Gx^2 + Gy^2)
+ //cv::magnitude(Gx, Gy, mag);
 
-/*double gx = 0.0;
-double gy = 0.0;
-double k=0.0;
- for ( int i = 0; i < theta.rows; i++ ){
-    for( int j = 0; j < theta.cols; j++ ){
-      gx = Gx.at<double>(i,j);
-      gy = Gy.at<double>(i,j);
-      if(gx !=0 && gy != 0){
-        k=atan2(gy,gx);
-      }else{
-        k=(double) atan(0);
-      }
-      theta.at<double>(i,j) =k;
-    }
-  }
-*/
- cv::Mat diff = angle3 != theta;
+
+
+ //cv::Mat diff = Angle != theta;
 // Equal if no elements disagree
 //bool eq = cv::countNonZero(diff) == 0;
 //cout<<"Eq: "<<eq<<endl;
+Mat Mag, Angle;
+//Mag.create(gray_image.size(),CV_64F);
+Angle.create(gray_image.size(),CV_32F);
+cartToPolar(Gx, Gy, Mag, Angle, 1);
+//imwrite( "Angle.jpg", Angle );
+// display images
 
- for ( int i = 0; i < mag.rows; i++ ){
-		for( int j = 0; j < mag.cols; j++ ){
-      double angle =atan2(Gy.at<double>(i,j),Gx.at<double>(i,j));
-      //angle = round(angle3.at<double>(i,j));
-      //angle = 0.4;
-      //cout<<"angle: "<<angle<<endl;
-      double radian;
-      if(angle<0){
-        radian = 360 + (angle*180/pi);
-      }else{
-        radian =  (angle*180/pi);
+float angle =0.0;
+float radian =0.0;
+float radians =0.0;
+float rho_value=0.0;
+float theta_value=0.0;
+float degree=0.0;
+int magThr=220;
+int angleRange =1;
+Mat MagThresh;
+MagThresh.create(Mag.size(),CV_32F);
+//vector<Vec2f> lines;
+
+int no_of_cols = 180;
+int no_of_rows = 1650;
+int initial_value = 0;
+
+std::vector<std::vector<float> > lines;
+lines.resize(no_of_rows, std::vector<float>(no_of_cols, initial_value));
+
+// Read from matrix.
+float value = lines[1][2];
+
+// Save to matrix.
+getThresholdedMag(Mag,MagThresh);
+ for ( int i = 0; i < Mag.rows; i++ ){
+		for( int j = 0; j < Mag.cols; j++ ){
+      //angle=outDir.at<double>(i,j);
+      if (MagThresh.at<float>(i, j) > 250) {
+        angle = Angle.at<float>(i, j);
+        if (angle > 0) radian = (angle * (180/pi));
+        else radian = 360 + (angle * (180/pi));
+
+        radian = round(radian);
+
+        for (int theta_value = 0; theta_value <180; theta_value ++) {
+        // for (int theta_value = 0; theta_value < 360; theta_value++){
+          radians = theta_value * (pi/ 180);
+          rho_value = (j * cos(radians)) + (i * sin(radians)) + rho_width + rho_height;
+          houghspaces.at<float>( rho_value , theta_value ) += 1;
+          //lines[rho_value][theta_value]++;
       }
-
-      //double degree= radian*180/pi;
-      double rho_value  = (i*cos(radian)) + (j*sin(radian)) + rho_width + rho_height;
-
-      //theta.at<double>(i, j)=angle;
-      rho.at<double>(i,j)=rho_value;
-
-      houghspaces.at<double>(round(rho_value),round(angle)) += 1;
-      //houghspaces.at<double>(rho,theta) += 1;
 		}
 	}
+}
 
-  for( int i = 0; i < houghspaces.rows; i++ ){
+normalize(houghspaces, houghspaces, 0, 255, NORM_MINMAX);
+imwrite( "5houghspaces.jpg", houghspaces );
+std::vector<float>  rhoValues;
+std::vector<float>  thetaValues;
+rhoValues.resize(25);
+thetaValues.resize(25);
+int count=0;
+for (int i = 0; i < houghspaces.rows; i++) {
+  for (int j = 0; j < houghspaces.cols; j++) {
+    float val = 0.0;
+    val = houghspaces.at<float>(i, j);
+    if (val > 150){
+      rhoValues[count]=i;
+      thetaValues[count]=j;
+      houghspaces.at<float>(i, j) = 255;
+      count++;
+      // std::cout<< rhoValues.size() << " + " << thetaValues.size() << "\n";
+    }
+
+    else houghspaces.at<float>(i, j) = 0.0;
+  }
+ }
+
+for( int i = 0; i < count; i++ )
+{
+    float rhos = rhoValues[i]-rho_width - rho_height, thetas = thetaValues[i];
+    float radians = thetas *pi/ 180;
+    //cout<<"rhos: "<<rhos<<", thetas: "<<radians<<endl;
+    Point pt1, pt2;
+    double a = cos(radians), b = sin(radians);
+    double x0 = a*rhos, y0 = b*rhos;
+    pt1.x = cvRound(x0 + 1000*(-b));
+    pt1.y = cvRound(y0 + 1000*(a));
+    pt2.x = cvRound(x0 - 1000*(-b));
+    pt2.y = cvRound(y0 - 1000*(a));
+    //cout<<"pt1.x: "<<pt1.x<<", pt1.y: "<<pt1.y<<", pt2.x: "<<pt2.x<<", pt2.y: "<<pt2.y<<endl;
+    line( image2, pt1, pt2, Scalar(0,0,255), 3, CV_AA);
+}
+
+
+
+
+  /*for( int i = 0; i < houghspaces.rows; i++ ){
     for( int j = 0; j < houghspaces.cols; j++ ){
-      if(houghspaces.at<double>(i,j)>90){
-         double rho2 = i-rho_width - rho_height;
-         double theta2 = j;
+      if(houghspaces.at<float>(i,j)>150 ){
+         //cout<<houghspaces.at<double>(i,j)<<endl;
+         float rho2 = i-rho_width - rho_height;
+         float theta2 = j;
 
          Point pt1, pt2;
-         double a = cos(theta2), b = sin(theta2);
-         double x0 = a*rho2, y0 = b*rho2;
+         float a = cos(theta2), b = sin(theta2);
+         float x0 = a*rho2, y0 = b*rho2;
 
          pt1.x = cvRound(x0 + 1000*(-b));
          pt1.y = cvRound(y0 + 1000*(a));
@@ -131,28 +200,68 @@ double k=0.0;
          line( image, pt1, pt2, Scalar(0,0,255), 3, CV_AA);
       }
      }
-   }
-
-double min_theta= find_min(theta);
-double max_theta= find_max(theta);
-double min_rho= find_min(rho);
-double max_rho= find_max(rho);
-/*cout<<"min_theta: "<<min_theta<<endl;
-cout<<"max_theta: "<<max_theta<<endl;
-cout<<"min_rho: "<<min_rho<<endl;
-cout<<"max_rho: "<<max_rho<<endl;*/
-
- imwrite( "edgeGx.jpg", Gx );
- imwrite( "edgeGY.jpg", Gy );
- imwrite( "mag.jpg", mag);
- imwrite( "mag3.jpg", mag3);
- imwrite( "angle3.jpg", angle3);
- imwrite( "houghspace.jpg", houghspaces);
- imwrite( "gray_image2.jpg", image);
- //imwrite( "gradient_direction.jpg", theta);
-
+   }*/
+ imwrite( "1edgeGx.jpg", Gx );
+ imwrite( "2edgeGY.jpg", Gy );
+ imwrite( "3image2.jpg", image2 );
+ imwrite( "4image.jpg", image);
+ imwrite( "6MagThresh.jpg", MagThresh);
+ imwrite( "7Mag.jpg", Mag);
+ imwrite( "8Angle.jpg", Angle);
  return 0;
 }
+
+void getMag(Mat &Gx,Mat &Gy,Mat &dst,Mat &img){
+  //Mat mm;
+  //mm.create(dst.size(),CV_64F);
+  for ( int i = 0; i < dst.rows; i++ ){
+ 		for( int j = 0; j < dst.cols; j++ ){
+      dst.at<float>(i,j) = sqrt(pow(Gx.at<float>(i,j),2)+pow(Gy.at<float>(i,j),2));
+    }
+  }
+  normalize(dst, dst, 0, 255, NORM_MINMAX);
+}
+
+void getDirection(Mat &Gx,Mat &Gy,Mat &dst){
+  float angle=0.0;
+  for ( int i = 0; i < dst.rows; i++ ){
+ 		for( int j = 0; j < dst.cols; j++ ){
+      if((Gx.at<float>(i,j) !=0) && (Gy.at<float>(i,j) != 0)){
+        angle =atan2(Gy.at<float>(i,j),Gx.at<float>(i,j));
+      }else{
+        angle = (float)atan(0.0);
+      }
+      dst.at<float>(i,j) = angle;
+    }
+  }
+  Mat img;
+	img.create(Gx.size(), CV_32F);
+
+
+  normalize(dst, dst, 0, 255, NORM_MINMAX);
+}
+
+void getThresholdedMag(Mat &input, Mat &output) {
+	Mat img;
+	img.create(input.size(), CV_32F);
+
+	normalize(input, img, 0, 255, NORM_MINMAX);
+
+	for (int y = 0; y < input.rows; y++) {
+    for (int x = 0; x < input.cols; x++) {
+
+      double val = 0;
+      val = img.at<float>(y, x);
+
+
+      if (val > 200) output.at<float>(y, x) = 255.0;
+      else output.at<float>(y, x) = 0.0;
+    }
+  }
+
+  imwrite("MagThresholded.jpg", output);
+}
+/*
 double find_min(Mat x){
   double min=x.at<double>(0,0);
   for ( int i = 0; i < x.rows; i++ ){
@@ -187,7 +296,7 @@ void normolization(Mat x,Mat y,double min_x,double max_x){
   }
   //cout<<"y.rows: "<<y.rows<<endl;
   //cout<<"y.cols: "<<y.cols<<endl;
-}
+}*/
 /*Mat convolve(cv::Mat &input, cv::Mat &kernel) {
 	cv::Mat blurredOutput;
 	int kernelRadiusX = ( kernel.size[0] - 1 ) / 2;
